520ca1351e4663f320cda67844a9f96d76532a9c
longnhat202142 <longnhat03082002@gmail.com>
2023-10-18 09:47:47 +0700

    Giới thiệu bản thân

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { NavBar } from './compoments/NavBar';
+import { Banner } from './compoments/Banner';
+import { Skills } from './compoments/Skills.js';
+import { Projects } from './compoments/Project.js';
+import { Footer } from './compoments/Footer.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/compoments/Project.js
@@ -0,0 +1,97 @@
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCards.js';
+import colorSharp2 from '../assets/img/color-sharp2.png';
+import projImg1 from '../assets/img/project-img1.png';
+import projImg2 from '../assets/img/project-img2.png';
+import projImg3 from '../assets/img/project-img3.png';
+
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg1,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg2,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg1,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg2,
+    },
+    {
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+    },
+  ];
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div className={isVisible ? 'animate__animated animate__bounce' : ''}>
+                  <h2>Project</h2>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book.
+                  </p>
+                </div>
+              )}
+            </TrackVisibility>
+
+            <Tab.Container id="project-tabs" defaultActiveKey="first">
+              <Nav
+                variant="pills"
+                defaultActiveKey="/home"
+                className="nav-pills mb-5 justify-content-center align-item-center"
+                id="pills-tab"
+              >
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab One</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                </Nav.Item>
+              </Nav>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                <Tab.Pane eventKey="third">Loren Event</Tab.Pane>
+              </Tab.Content>
+            </Tab.Container>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} />
+    </section>
+  );
+};
